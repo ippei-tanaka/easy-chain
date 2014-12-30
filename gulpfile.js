45f926b5f53e9c13814e35920c440737bee8a4cb
Ippei <tanaka.ippei.jp@gmail.com>
2014-12-30 13:02:05 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp'),
+    karma  = require('gulp-karma'),
+    notify = require('gulp-notify'),
+    runSequence = require('run-sequence'),
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
+    rename = require("gulp-rename");
+
+gulp.task('default', function (next) {
+    runSequence(
+        'test-with-jquery1',
+        'test-with-jquery2',
+        'clean',
+        'build'
+    );
+    next();
+});
+
+gulp.task('test', function (next) {
+    runSequence(
+        'test-with-jquery1'
+    );
+    next();
+});
+
+
+gulp.task('build', function () {
+    return gulp.src('src/*.js')
+        .pipe(gulp.dest('dist'))
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean', function () {
+    return gulp.src('dist')
+        .pipe(clean({force: true}));
+});
+
+gulp.task('test-with-jquery1', function () {
+    return gulp
+        .src([
+            './lib/jquery-1.11.2.js',
+            './lib/underscore.js',
+            './src/*.js',
+            './test/*.js'
+        ])
+        .pipe(karma({
+            configFile: 'karma.conf.js',
+            action: 'run'
+        }))
+        .on('error', notify.onError({
+            title: 'Jasmine Test Failed',
+            message: 'One or more tests failed, see the cli for details.'
+        }));
+});
+
+gulp.task('test-with-jquery2', function () {
+    return gulp
+        .src([
+            './lib/jquery-2.1.3.js',
+            './lib/underscore.js',
+            './src/*.js',
+            './test/*.js'
+        ])
+        .pipe(karma({
+            configFile: 'karma.conf.js',
+            action: 'run'
+        }))
+        .on('error', notify.onError({
+            title: 'Jasmine Test Failed',
+            message: 'One or more tests failed, see the cli for details.'
+        }));
+});
---
+++ test/test.js
@@ -0,0 +1,367 @@
+describe("EasyChain", function () {
+
+    var originalTimeout,
+        chain,
+        chain2,
+        chain3,
+        spiedFunction,
+        timeout = 50;
+
+    beforeEach(function () {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 500;
+        chain = new EasyChain({
+            timeout: timeout
+        });
+        chain2 = new EasyChain();
+        chain3 = new EasyChain();
+        spiedFunction = jasmine.createSpy('spiedFunction');
+    });
+
+    it("should run sequentially functions.", function () {
+        chain
+            .single(function (next) {
+                next();
+            })
+            .single(function (next) {
+                spiedFunction();
+                next();
+            })
+            .run();
+        expect(spiedFunction).toHaveBeenCalled();
+    });
+
+    it("should not run functions that doesn't invoke the given callback.", function () {
+        chain
+            .single(function () {})
+            .single(function () {
+                spiedFunction();
+            })
+            .run();
+
+        chain2
+            .first(function () {}, function () {}, function () {})
+            .single(function () {
+                spiedFunction();
+            })
+            .run();
+
+        chain3
+            .all(function () {}, function (next) { next() }, function (next) { next() })
+            .single(function () {
+                spiedFunction();
+            })
+            .run();
+
+        expect(spiedFunction).not.toHaveBeenCalled();
+    });
+
+    it("should run all functions in parallel.", function () {
+        chain
+            .all(function (next) {
+                spiedFunction();
+                next();
+            }, function (next) {
+                spiedFunction();
+                next();
+            })
+            .all([function (next) {
+                spiedFunction();
+                next();
+            }, function (next) {
+                spiedFunction();
+                next();
+            }])
+            .single(spiedFunction)
+            .run();
+        expect(spiedFunction.calls.count()).toEqual(5);
+    });
+
+    it("should run first function in parallel.", function () {
+        chain
+            .first(function (next) {
+                next();
+            }, function (next) {
+            }, function (next) {
+            })
+            .first([function (next) {
+                next();
+            }, function (next) {
+                next();
+            }, function (next) {
+            }])
+            .single(spiedFunction)
+            .run();
+        expect(spiedFunction).toHaveBeenCalled();
+    });
+
+
+    it("should run functions that returns a jQuery promise.", function () {
+        var deferredObjects = _(new Array(8)).map(function () { return $.Deferred(); })
+
+        chain
+            .single(function () {
+                return deferredObjects[0].promise();
+            })
+            .all(function () {
+                return deferredObjects[1].promise();
+            })
+            .first(function () {
+                return deferredObjects[1].promise();
+            })
+            .all([function () {
+                return deferredObjects[1].promise();
+            }])
+            .first([function () {
+                return deferredObjects[1].promise();
+            }])
+            .first(
+                function () {
+                    var p = deferredObjects[2].promise();
+                    deferredObjects[2].done(spiedFunction);
+                    return p;
+                },
+                function () {
+                    var p = deferredObjects[3].promise();
+                    deferredObjects[3].done(spiedFunction);
+                    return p;
+                }
+            )
+            .all(
+                deferredObjects[4].promise,
+                deferredObjects[5].promise,
+                deferredObjects[6].promise,
+                function (next) {
+                    next();
+                    spiedFunction();
+                }
+            )
+            .single(function () {
+                spiedFunction();
+                return deferredObjects[7].promise();
+            })
+            .run();
+
+        deferredObjects[0].resolve();
+        deferredObjects[1].resolve();
+        deferredObjects[2].resolve();
+        deferredObjects[4].resolve();
+        deferredObjects[5].resolve();
+        deferredObjects[6].resolve();
+
+        expect(spiedFunction.calls.count()).toEqual(3);
+    });
+
+    it("should fire complete callback.", function () {
+        chain
+            .single(function (next) { next() })
+            .single(function (next) { next() })
+            .on(EasyChain.COMPLETE, function () {
+                spiedFunction();
+            })
+            .off(EasyChain.COMPLETE)
+            .on(EasyChain.COMPLETE, function (event) {
+                expect(event.length).toBe(2);
+                expect(event.target).toBe(chain);
+                expect(event.type).toBe(EasyChain.COMPLETE);
+                spiedFunction();
+            })
+            .run();
+
+        expect(spiedFunction.calls.count()).toEqual(1);
+    });
+
+    it("should fire progress callback.", function () {
+        var callback = function () {
+            var args = arguments;
+            return function (next) { next.apply({}, args) };
+        };
+
+        chain
+            .single(callback("Hi", "Hello"))
+            .all(callback("H", "E"), callback("L"), callback("L", "O"))
+            .first(callback("H", "E"), callback("L"), callback("L", "O"))
+            .on(EasyChain.PROGRESS, function (event) {
+                expect(event.type).toBe(EasyChain.PROGRESS);
+                expect(event.target).toBe(chain);
+                if (event.index === 0) {
+                    expect(event.queueType).toBe('single');
+                    expect(event.values).toEqual(["Hi", "Hello"]);
+                } else if (event.index === 1) {
+                    expect(event.queueType).toBe('all');
+                    expect(event.values).toEqual([["H", "E"], ["L"], ["L", "O"]]);
+                } else if (event.index === 2) {
+                    expect(event.queueType).toBe('first');
+                    expect(event.values).toEqual([["H", "E"], undefined, undefined]);
+                }
+                spiedFunction();
+            })
+            .run();
+
+        expect(spiedFunction.calls.count()).toEqual(3);
+    });
+
+    it("should trigger error event when timeout occurs.", function (done) {
+        chain
+            .all(function (next) { next() })
+            .all(function (next) { next("H", "I") }, function (next) { })
+            .on(EasyChain.TIMEOUT_ERROR, function (event) {
+                expect(event.type).toBe(EasyChain.TIMEOUT_ERROR);
+                expect(event.target).toBe(chain);
+                expect(event.values).toEqual([["H", "I"], undefined]);
+                spiedFunction();
+            })
+            .run();
+
+        setTimeout(function () {
+            expect(spiedFunction).toHaveBeenCalled();
+            done();
+        }, timeout + 50)
+    });
+
+    it("should not trigger error event when timeout doesn't occur.", function (done) {
+        chain
+            .single(function (next) { next() })
+            .on(EasyChain.TIMEOUT_ERROR, function () {
+                spiedFunction();
+            })
+            .run();
+
+        setTimeout(function () {
+            expect(spiedFunction).not.toHaveBeenCalled();
+            done();
+        }, timeout + 50)
+    });
+
+    it("should run nested chains.", function () {
+        var callback = function (next) { next(); };
+
+        chain
+            .all(
+                chain2
+                    .all(callback, callback, callback),
+                chain3
+                    .all(callback, callback, callback)
+                    .first(callback, callback, callback),
+                function (next) {
+                    next();
+                }
+            )
+            .single(function () {
+                spiedFunction();
+            })
+            .run();
+
+        expect(spiedFunction).toHaveBeenCalled();
+    });
+
+    it("should wait.", function (done) {
+        var callback = function (next) { next(); };
+
+        chain
+            .wait(200)
+            .all(callback, callback, callback)
+            .single(function () {
+            })
+            .on(EasyChain.PROGRESS, function (event) {
+                if (event.index === 0) {
+                    spiedFunction();
+                    expect(event.queueType).toBe('wait');
+                    expect(event.values).toEqual(200);
+                }
+            })
+            .run();
+
+        expect(spiedFunction).not.toHaveBeenCalled();
+
+        setTimeout(function () {
+            expect(spiedFunction).not.toHaveBeenCalled();
+        }, 10);
+
+        setTimeout(function () {
+            expect(spiedFunction).toHaveBeenCalled();
+            done();
+        }, 300);
+    });
+
+    it("should not complete incorrectly nested chains.", function () {
+        var callback = function (next) { next(); };
+
+        chain
+            .all(
+                chain2
+                    .all(callback, callback, callback),
+                chain3
+                    .all(callback, callback, callback)
+                    .first(function () {}, function () {})
+            )
+            .single(function () {
+                spiedFunction();
+            })
+            .run();
+
+        expect(spiedFunction).not.toHaveBeenCalled();
+    });
+
+    it("should on and off event-listeners.", function () {
+        var obj = {},
+            callback =  function () {
+                spiedFunction();
+            };
+
+        chain
+            .single(function (next) { next() })
+            .on(EasyChain.COMPLETE, callback, obj)
+            .on(EasyChain.COMPLETE, function () {}, obj)
+            .off(EasyChain.COMPLETE, callback, {})
+            .run();
+
+        expect(spiedFunction).not.toHaveBeenCalled();
+
+        chain
+            .empty()
+            .single(function (next) { next() })
+            .on(EasyChain.COMPLETE, callback)
+            .on(EasyChain.COMPLETE, callback)
+            .off(EasyChain.COMPLETE, callback)
+            .run();
+
+        expect(spiedFunction).not.toHaveBeenCalled();
+
+        chain
+            .empty()
+            .single(function (next) { next() })
+            .on(EasyChain.PROGRESS, callback)
+            .off(EasyChain.PROGRESS)
+            .run();
+
+        expect(spiedFunction).not.toHaveBeenCalled();
+
+    });
+
+    it("should return instance through static methods.", function (done) {
+        var callback = function (next) { next(); };
+
+        EasyChain
+            .all(callback, callback, callback, EasyChain.first([callback, callback]))
+            .first(
+                EasyChain.wait(100),
+                function () {}
+            )
+            .single(function () {
+                spiedFunction();
+            })
+            .run();
+
+        expect(spiedFunction).not.toHaveBeenCalled();
+
+        setTimeout(function () {
+            expect(spiedFunction).toHaveBeenCalled();
+            done();
+        }, 300);
+    });
+
+    afterEach(function () {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+});
